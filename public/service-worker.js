e30e7f5643b9ad5fb66b344d71543a4285bf91f6
omer lev <omerlev66@gmail.com>
2021-10-02 21:33:14 +0300

    SW Setup

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
 
 let defferedPrompt;
-const popUp = document.getElementsByClassName('.install-app');
+const popUp = $('.install-app');
 
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
