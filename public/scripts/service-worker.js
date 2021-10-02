20ae2e19ba4d83e60d1bb3c731b4be8d44896ef6
omer lev <omerlev66@gmail.com>
2021-10-02 21:42:58 +0300

    SW NetworkFirst mod

--- public/scripts/service-worker.js
+++ public/scripts/service-worker.js
@@ -2,5 +2,5 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox
 
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
-    new workbox.strategies.CacheFirst()
+    new workbox.strategies.NetworkFirst()
 );
