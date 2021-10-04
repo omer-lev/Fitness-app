fcb6ba4d8cdb0cd59a872bb07ca696369ff4cbba
omer lev <omerlev66@gmail.com>
2021-10-04 22:04:02 +0300

    login/register responsive

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -4,6 +4,7 @@ const times = $('.drop-down .fa-times');
 const mobileNav = $('.mobile-nav');
 const popUp = $('.install-app');
 const closePopUp = $('.install-app .fa-times');
+const installAppBtn = $('.install-app button');
 let defferedPrompt;
 
 logo.on('click', () => {
@@ -26,7 +27,7 @@ closePopUp.on('click', () => {
     popUp.css('display', 'none');
 });
 
-
+// APP INSTALLATION
 window.addEventListener('beforeinstallprompt', (e) => {
     e.preventDefault();
     defferedPrompt = e;
@@ -34,4 +35,23 @@ window.addEventListener('beforeinstallprompt', (e) => {
     popUp.css('display', 'flex');
 
     console.log(`--> DEFFERED PROMPT: ${defferedPrompt}`);
+});
+
+installAppBtn.on('click', async () => {
+    if (!defferedPrompt)
+        return;
+    
+    defferedPrompt.prompt();
+
+    const result = await defferedPrompt.userChoice;
+    console.log(`--> USER CHOICE: ${result}`);
+
+    defferedPrompt = null;
+
+    popUp.css('display', 'none');
+});
+
+window.addEventListener('appinstalled', (e) => {
+    console.log(`--> APP INSTALLED: ${e}`);
+    defferedPrompt = null;
 });
