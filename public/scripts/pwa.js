7c7789ed3ed21eabffae7e7937a327f25e350fb6
omer lev <omerlev66@gmail.com>
2021-10-05 16:52:26 +0300

    pwa.js script

---
+++ public/scripts/pwa.js
@@ -0,0 +1,33 @@
+let defferedPrompt;
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js');
+}
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    defferedPrompt = e;
+
+    popUp.css('display', 'flex');
+
+    console.log(`--> DEFFERED PROMPT: ${defferedPrompt}`);
+});
+
+installAppBtn.on('click', async () => {
+    if (!defferedPrompt)
+        return;
+    
+    defferedPrompt.prompt();
+
+    const result = await defferedPrompt.userChoice;
+    console.log(`--> USER CHOICE: ${result}`);
+
+    defferedPrompt = null;
+
+    popUp.css('display', 'none');
+});
+
+window.addEventListener('appinstalled', (e) => {
+    console.log(`--> APP INSTALLED: ${e}`);
+    defferedPrompt = null;
+});
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,7 +5,6 @@ const mobileNav = $('.mobile-nav');
 const popUp = $('.install-app');
 const closePopUp = $('.install-app .fa-times');
 const installAppBtn = $('.install-app button');
-let defferedPrompt;
 
 logo.on('click', () => {
     window.location.href = '/';
@@ -25,33 +24,4 @@ times.on('click', () => {
 
 closePopUp.on('click', () => {
     popUp.css('display', 'none');
-});
-
-// APP INSTALLATION
-window.addEventListener('beforeinstallprompt', (e) => {
-    e.preventDefault();
-    defferedPrompt = e;
-
-    popUp.css('display', 'flex');
-
-    console.log(`--> DEFFERED PROMPT: ${defferedPrompt}`);
-});
-
-installAppBtn.on('click', async () => {
-    if (!defferedPrompt)
-        return;
-    
-    defferedPrompt.prompt();
-
-    const result = await defferedPrompt.userChoice;
-    console.log(`--> USER CHOICE: ${result}`);
-
-    defferedPrompt = null;
-
-    popUp.css('display', 'none');
-});
-
-window.addEventListener('appinstalled', (e) => {
-    console.log(`--> APP INSTALLED: ${e}`);
-    defferedPrompt = null;
 });
