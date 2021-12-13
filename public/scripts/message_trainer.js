c93d71ded4bb06a864e0e8fd2b9f7f96d6dbe051
omer lev <omerlev66@gmail.com>
2021-12-13 16:49:31 +0200

    V2 final touches

--- public/scripts/message_trainer.js
+++ public/scripts/message_trainer.js
@@ -1,8 +1,12 @@
+const trainerForm = $('.profile .profile-card form.section');
 const formBtn = $('.profile .profile-card form.section button');
 const formInpt = $('.profile .profile-card form.section input')[0];
 const messageConfirmation = $('.profile .profile-card .section#message-confirmation');
+const messageConfirmationTxt = $('.profile .profile-card .section#message-confirmation p');
 
-formBtn.on('click', async (e) => {
+formBtn.on('click', async () => {
+    if (formInpt.value == "") return;
+
     formBtn[0].innerText = "...";
 
     $.ajax({
@@ -15,8 +19,18 @@ formBtn.on('click', async (e) => {
     });
 
     formBtn[0].innerText = "send";
+});
 
+trainerForm.on('submit', (e) => {
+    e.preventDefault();
+
+    formInpt.value = "";
     messageConfirmation.css('display', 'flex');
+});
+
+messageConfirmationTxt.on('click', () => {
+    console.log('clicked');
+    messageConfirmation.css('display', 'none');
 });
 
 $(document).ready(() => {
